e9c3af1da127bf30da697cd4a8d8b5235882940c
jonahokot <jonahrokot2@gmail.com>
2022-09-15 10:39:21 +0300

    deleted:    test/controllers/produceController.js

--- test/controllers/produceController.js
+++
@@ -1,9 +0,0 @@
-const { assert } = require('chai');
-const { describe, it } = require('mocha');
-const produceController = require('../../controllers/produceController');
-
-describe('Produce Controller', () => {
-  it('should get produce', () => {
-    assert.isNotEmpty(produceController.getAllProduce);
-  });
-});
